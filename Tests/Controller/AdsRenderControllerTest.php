f822d919c8737320f369200949116392b640b4a6
Isaac Gasi <isaac.gasi@gmail.com>
2017-08-08 17:56:40 -0600

    Implemented coding standards on Test files.

--- Tests/Controller/AdsRenderControllerTest.php
+++ Tests/Controller/AdsRenderControllerTest.php
@@ -1,16 +1,8 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\ads_system\Tests\AdsRenderController.
- */
-
 namespace Drupal\ads_system\Tests;
 
 use Drupal\simpletest\WebTestBase;
-use Drupal\Core\Entity\EntityManager;
-use Drupal\Core\Entity\Query\QueryFactory;
-use Drupal\Core\Form\FormAjaxResponseBuilder;
 
 /**
  * Provides automated tests for the ads_system module.
@@ -22,37 +14,31 @@ class AdsRenderControllerTest extends WebTestBase {
    *
    * @var Drupal\Core\Entity\EntityManager
    */
-  protected $entity_manager;
+  protected $entityManager;
 
   /**
    * Drupal\Core\Entity\Query\QueryFactory definition.
    *
    * @var Drupal\Core\Entity\Query\QueryFactory
    */
-  protected $entity_query;
+  protected $entityQuery;
 
   /**
    * Drupal\Core\Form\FormAjaxResponseBuilder definition.
    *
    * @var Drupal\Core\Form\FormAjaxResponseBuilder
    */
-  protected $form_ajax_response_builder;
-  /**
-   * {@inheritdoc}
-   */
-  public static function getInfo() {
-    return array(
-      'name' => "ads_system AdsRenderController's controller functionality",
-      'description' => 'Test Unit for module ads_system and controller AdsRenderController.',
-      'group' => 'Other',
-    );
-  }
+  protected $formAjaxResponseBuilder;
 
   /**
    * {@inheritdoc}
    */
-  public function setUp() {
-    parent::setUp();
+  public static function getInfo() {
+    return [
+      'name' => "ads_system AdsRenderController's controller functionality",
+      'description' => 'Test Unit for module ads_system and controller AdsRenderController.',
+      'group' => 'ads_system',
+    ];
   }
 
   /**
